c99b0fc37c87e97461951ba63b67959832426fa5
rishiprakash <rishiprakash99@gmail.com>
2022-01-03 20:32:41 +0530

    Adding new examples of multiple design patterns

--- src/main/java/OOD/ShoppingCart/Client.java
+++ src/main/java/OOD/ShoppingCart/Client.java
@@ -1,7 +1,22 @@
 package OOD.ShoppingCart;
 
+import OOD.ShoppingCart.Cart.ICart;
+import OOD.ShoppingCart.Cart.ShoppingCart;
+import OOD.ShoppingCart.Entity.Product;
+import OOD.ShoppingCart.Entity.ProductCategory;
+import OOD.ShoppingCart.Inventory.Catalog;
+
 public class Client {
     public static void main(String[] args) {
+        Catalog catalog = new Catalog();
+        catalog.addProduct(new Product.Builder().withID("1").withName("Book-OOPS").withPrice(200).withCategory(ProductCategory.BOOK).build());
+        catalog.addProduct(new Product.Builder().withID("2").withName("Book-Python").withPrice(200).withCategory(ProductCategory.BOOK).build());
+        catalog.addProduct(new Product.Builder().withID("3").withName("Book-Azure").withPrice(200).withCategory(ProductCategory.BOOK).build());
 
+
+        Product product = catalog.lookUp("Book-Azure");
+        ICart cart = new ShoppingCart();
+        cart.addItem(product);
+        cart.removeItem(product.getName());
     }
 }
